0ab2c8e69b79b5741f1bca682416797ef9d84b69
Miguel Ángel Roca González <miguelangel.roca@iesdonana.org>
2023-02-06 00:30:20 +0100

    Actividad 3 Unidad 6 Terminada

--- T2/U6/A03-U06-Roca.js
+++ T2/U6/A03-U06-Roca.js
@@ -7,49 +7,65 @@ class Vehiculo {
     this.cilindrada = cilindrada;
   }
   mostrarDatos() {
-    var div = document.getElementById("tabla");
-    var table = document.createElement("table");
-    var thead = document.createElement("thead");
-    var tr = document.createElement("tr");
-    var th1 = document.createElement("th");
-    var text = document.createTextNode("Marca");
-    th1.appendChild(text);
-    var th2 = document.createElement("th");
-    var text = document.createTextNode("Modelo");
-    th2.appendChild(text);
-    var th3 = document.createElement("th");
-    var text = document.createTextNode("Color");
-    th3.appendChild(text);
-    var th4 = document.createElement("th");
-    var text = document.createTextNode("Año");
-    th4.appendChild(text);
-    var th5 = document.createElement("th");
-    var text = document.createTextNode("Cilindrada");
-    th5.appendChild(text);
+    if (!document.getElementById("tabla")) {
+      var table = document.createElement("table");
+      table.setAttribute("id", "tabla");
 
-    tr.appendChild(th1);
-    tr.appendChild(th2);
-    tr.appendChild(th3);
-    tr.appendChild(th4);
-    tr.appendChild(th5);
-    thead.appendChild(tr);
-    table.appendChild(thead);
-
-    var tbody = document.createElement("tbody");
-    for (var i = 0; i < 5; i++) {
       var tr = document.createElement("tr");
-      var values = Object.keys(this);
+      var th1 = document.createElement("th");
+      var text = document.createTextNode("Marca");
+      th1.appendChild(text);
+      tr.appendChild(th1);
+
+      var th2 = document.createElement("th");
+      var text = document.createTextNode("Modelo");
+      th2.appendChild(text);
+      tr.appendChild(th2);
+
+      var th3 = document.createElement("th");
+      var text = document.createTextNode("Color");
+      th3.appendChild(text);
+      tr.appendChild(th3);
+
+      var th4 = document.createElement("th");
+      var text = document.createTextNode("Año");
+      th4.appendChild(text);
+      tr.appendChild(th4);
+
+      var th5 = document.createElement("th");
+      var text = document.createTextNode("Cilindrada");
+      th5.appendChild(text);
+      tr.appendChild(th5);
+
+      var thead = document.createElement("thead");
+      thead.appendChild(tr);
+
+      table.appendChild(thead);
+
+      var tbody = document.createElement("tbody");
+      tbody.setAttribute("id", "tbody");
+
+      table.appendChild(tbody);
+      document.getElementById("container").appendChild(table);
+    } else {
+      var table = document.getElementById("tabla");
+
       for (var i = 0; i < 5; i++) {
-        var value = values[i];
-        var td = document.createElement("td");
-        var text = document.createTextNode(`${this[value]}`);
-        td.appendChild(text);
-        tr.appendChild(td);
+        var tr = document.createElement("tr");
+        var values = Object.keys(this);
+        for (var i = 0; i < 5; i++) {
+          var value = values[i];
+          var td = document.createElement("td");
+          var text = document.createTextNode(`${this[value]}`);
+          td.appendChild(text);
+          tr.appendChild(td);
+        }
+        var tbody = document.getElementById("tbody");
+        tbody.appendChild(tr);
       }
-      tbody.appendChild(tr);
+      table.appendChild(tbody);
+      document.getElementById("container").appendChild(table);
     }
-    table.appendChild(tbody);
-    div.appendChild(table);
   }
 }
 var objeto1 = new Vehiculo("Ford", "Focus", "Rojo", "2020", 1600);
@@ -58,8 +74,10 @@ var objeto3 = new Vehiculo("Renault", "Megane", "Verde", "2020", 1500);
 var objeto4 = new Vehiculo("Peugeot", "508", "Blanco", "2021", 2000);
 var objeto5 = new Vehiculo("Toyota", "Yaris", "Azul", "2017", 1200);
 
-objeto1.mostrarDatos();
-objeto2.mostrarDatos();
-objeto3.mostrarDatos();
-objeto4.mostrarDatos();
-objeto5.mostrarDatos();
+var objetos = [objeto1, objeto2, objeto3, objeto4, objeto5];
+
+document.getElementById("generar").addEventListener("click", () => {
+  objetos.forEach((objeto) => {
+    objeto.mostrarDatos();
+  });
+});
